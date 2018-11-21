2055473d018cec271742608caa9dfda7de9bc7c9
YangKui <c6swift@gmail.com>
2018-11-21 22:42:10 +0800

    fix: 看来 npm 不能使用 mjs?

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-const usePolling = require('./index.mjs');
+function usePolling(api, cycleMs, predicate = () => true) {
+  if (typeof api !== 'function') {
+    throw TypeError('You should pass a function as the first param.');
+  }
+  if (typeof cycleMs !== 'number') {
+    throw TypeError('You should pass the ms for the cycle of polling.');
+  }
+  if (typeof predicate !== 'function') {
+    throw TypeError('The predicate is used to determine when to exit polling');
+  }
+
+  const wait = () => new Promise(r => setTimeout(r, cycleMs));
+  let stop = false;
+
+  const pollingHelper = async function*() {
+    let res = null;
+    while (!stop) {
+      res = await api();
+      if (predicate(res)) {
+        return res;
+      }
+      yield res;
+      await wait();
+    }
+  };
+
+  return async fp => {
+    for await (const res of pollingHelper()) {
+      fp(res);
+    }
+  };
+}
 
 module.exports = usePolling;
