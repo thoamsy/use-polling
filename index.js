6cbfc722884800b47b302beeeebd2627773a695c
YangKui <c6swift@gmail.com>
2018-11-25 14:30:16 +0800

    refactor: 支持 curry

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-function usePolling(api) {
+const curry = require('lodash.curry');
+function usePolling(api, { cycleMs, predicate = () => true }) {
   if (typeof api !== 'function') {
     throw TypeError('You should pass a function as the first param.');
   }
 
-  return ({ cycleMs, predicate = () => true }) => {
-    if (typeof cycleMs !== 'number') {
-      throw TypeError('You should pass the ms for the cycle of polling.');
-    }
-    if (typeof predicate !== 'function') {
-      throw TypeError(
-        'The predicate is used to determine when to exit polling',
-      );
-    }
+  if (typeof cycleMs !== 'number') {
+    throw TypeError('You should pass the ms for the cycle of polling.');
+  }
+  if (typeof predicate !== 'function') {
+    throw TypeError('The predicate is used to determine when to exit polling');
+  }
 
-    const wait = () => new Promise(r => setTimeout(r, cycleMs));
+  const wait = () => new Promise(r => setTimeout(r, cycleMs));
 
-    const pollingHelper = async function*() {
-      while (true) {
-        const res = await api();
-        yield res;
-        if (predicate(res)) {
-          return res;
-        }
-        await wait();
+  const pollingHelper = async function*() {
+    while (true) {
+      const res = await api();
+      yield res;
+      if (predicate(res)) {
+        return res;
       }
-    };
+      await wait();
+    }
+  };
 
-    return async fp => {
-      for await (const res of pollingHelper()) {
-        fp(res);
-      }
-    };
+  return async fp => {
+    for await (const res of pollingHelper()) {
+      fp(res);
+    }
   };
 }
 
-export default usePolling;
+export default curry(usePolling);
